31675a3dc90ae2db1a18cf718ba1a5c7d9557cd6
hirahira <th11282@gmail.com>
2025-12-10 09:10:58 +0900

    改善

--- BlazorBooksPoc/Data/AppDbContext.cs
+++ BlazorBooksPoc/Data/AppDbContext.cs
@@ -3,12 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BlazorBooksPoc.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
-
     public DbSet<Book> Books { get; set; } = default!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
